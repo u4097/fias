7c747fe3d004f31273e4175b5bbdd32b21cadd20
sitnikov <oleg.sitnikov@icloud.com>
2019-12-16 22:18:16 +0300

    Add update module

--- src/main/java/ru/bazis/fias/Application.java
+++ src/main/java/ru/bazis/fias/Application.java
@@ -10,7 +10,11 @@ public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
 
+      try {
+          UpdateFias.update();
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
   }
-
 
 }
